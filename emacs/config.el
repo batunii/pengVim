5ae99356c39aef40f2dbdcad228880004cdfa7d8
batunii <sy.soni@outlook.com>
2026-06-06 16:58:48 +0100

    New Config changes for Niri and Emacs

--- emacs/config.el
+++ emacs/config.el
@@ -1,43 +1,34 @@
-;; Increase GC threshold during startup (prevents excessive GCs)
 (setq gc-cons-threshold most-positive-fixnum
       gc-cons-percentage 0.6)
 
-;; Disable file name handlers during startup (big speed boost)
 (defvar batunii--file-name-handler-alist file-name-handler-alist)
 (setq file-name-handler-alist nil)
 
-;; Restore after Elpaca finishes (not just emacs-startup-hook)
 (add-hook 'elpaca-after-init-hook
           (lambda ()
-            (setq gc-cons-threshold (* 20 1024 1024)  ; 16MB
+            (setq gc-cons-threshold (* 20 1024 1024)
                   gc-cons-percentage 0.1
                   file-name-handler-alist batunii--file-name-handler-alist)))
 
-;; Fallback: also restore on emacs-startup-hook if elpaca hook doesn't exist
 (add-hook 'emacs-startup-hook
           (lambda ()
             (setq gc-cons-threshold (* 20 1024 1024)
                   gc-cons-percentage 0.1
                   file-name-handler-alist batunii--file-name-handler-alist)))
 
-;; Don't compact font caches during GC
 (setq inhibit-compacting-font-caches t)
-
-;; Reduce frame resize overhead during startup
 (setq frame-inhibit-implied-resize t)
+(setq read-process-output-max (* 1024 1024))
 
-;; Increase amount of data Emacs reads from processes
-(setq read-process-output-max (* 1024 1024))  ; 1MB
-
-(defvar elpaca-installer-version 0.11)
+(defvar elpaca-installer-version 0.12)
 (defvar elpaca-directory (expand-file-name "elpaca/" user-emacs-directory))
 (defvar elpaca-builds-directory (expand-file-name "builds/" elpaca-directory))
-(defvar elpaca-repos-directory (expand-file-name "repos/" elpaca-directory))
+(defvar elpaca-sources-directory (expand-file-name "sources/" elpaca-directory))
 (defvar elpaca-order '(elpaca :repo "https://github.com/progfolio/elpaca.git"
                               :ref nil :depth 1 :inherit ignore
                               :files (:defaults "elpaca-test.el" (:exclude "extensions"))
-                              :build (:not elpaca--activate-package)))
-(let* ((repo  (expand-file-name "elpaca/" elpaca-repos-directory))
+                              :build (:not elpaca-activate)))
+(let* ((repo  (expand-file-name "elpaca/" elpaca-sources-directory))
        (build (expand-file-name "elpaca/" elpaca-builds-directory))
        (order (cdr elpaca-order))
        (default-directory repo))
@@ -71,14 +62,14 @@
 (elpaca elpaca-use-package
   (elpaca-use-package-mode))
 
-(use-package emacs 
-  :ensure nil 
-  :config 
+(use-package emacs
+  :ensure nil
+  :config
   (setq ring-bell-function #'ignore)
   (setq shift-select-mode nil)
   (setq org-support-shift-select nil))
 
-(use-package evil 
+(use-package evil
   :ensure (:wait t)
   :init
   (setq evil-want-integration t
@@ -86,11 +77,11 @@
         evil-want-minibuffer t
         evil-vsplit-window-right t
         evil-split-window-below t
-	  evil-undo-system 'undo-redo)
+        evil-undo-system 'undo-redo)
   :config
   (evil-mode 1))
 
-(use-package evil-collection 
+(use-package evil-collection
   :ensure (:wait t)
   :after evil
   :config
@@ -98,11 +89,11 @@
   (setq evil-collection-want-unimpaired-p nil)
   (evil-collection-init))
 
-(use-package general 
+(use-package general
   :ensure (:wait t)
   :config
   (general-evil-setup)
-  
+
   (general-create-definer batunii/leader-keys
     :states '(normal insert visual emacs)
     :keymaps 'override
@@ -136,15 +127,12 @@
 
 (batunii/leader-keys
   "sr" '(query-replace :wk "Query replace")
-  "e" '(treemacs :wk "Treemacs")
-  "cc" '(compile :wk "Compile")
-  )
+  "e"  '(treemacs :wk "Treemacs")
+  "cc" '(compile :wk "Compile"))
 
-;; Set default frame parameters (applies to all frames)
 (setq default-frame-alist
-      '((font . "JetBrainsMono Nerd Font-15")))  ; Note: size is part of font string
+      '((font . "JetBrainsMono Nerd Font-15")))
 
-;; Additional font settings
 (set-face-attribute 'variable-pitch nil
                     :font "JetBrainsMono Nerd Font"
                     :height 190
@@ -165,21 +153,19 @@
   (catppuccin-italic-variables nil)
   (catppuccin-italic-blockquotes t)
   (catppuccin-flavor 'mocha)
-  
   :config
   (load-theme 'catppuccin :no-confirm)
-  
-  ;; Override background colors
+
   (catppuccin-set-color 'base "#181818")
   (catppuccin-set-color 'mantle "#282828")
   (catppuccin-set-color 'crust "#282828")
-  
+
   (catppuccin-reload))
 
 (menu-bar-mode -1)
 (tool-bar-mode -1)
 (scroll-bar-mode -1)
-(set-frame-parameter nil 'undecorated t) ; Remove window decorations
+(set-frame-parameter nil 'undecorated t)
 
 (setq display-line-numbers-type 'relative)
 (global-display-line-numbers-mode +1)
@@ -228,77 +214,116 @@
           (js-mode     . js-ts-mode)
           (css-mode    . css-ts-mode)
           (json-mode   . json-ts-mode)))
-  
+
   (setq treesit-font-lock-level 4))
 
+;; basedpyright is installed system-wide (pacman -S python-basedpyright).
+;; pet.el detects the active Python interpreter (conda env, venv, pyenv, uv)
+;; and silently tells pyright which one to use for import resolution.
+(use-package pet
+  :ensure t
+  :hook (python-ts-mode . (lambda ()
+                            (pet-mode)
+                            (setq-local lsp-pyright-python-executable-cmd
+                                        (pet-executable-find "python"))
+                            (setq-local lsp-pyright-venv-path
+                                        (pet-virtualenv-root)))))
+
+;; lsp-pyright MUST come before lsp-mode so the client is registered first.
 (use-package lsp-pyright
-:ensure t
-:hook (python-ts-mode . (lambda () (require 'lsp-pyright) (lsp))))
+  :ensure t
+  :custom
+  (lsp-pyright-langserver-command "basedpyright"))
+
 (use-package lsp-mode
   :ensure t
-  :commands lsp
+  :commands lsp lsp-deferred
   :diminish (lsp-mode . " LSP")
-  :hook ((python-ts-mode . lsp)
-         (c-ts-mode . lsp)
-         (c++-ts-mode . lsp)
-         (js-ts-mode . lsp)
+  :hook ((python-ts-mode . (lambda ()
+                              (require 'lsp-pyright)
+                              (lsp-deferred)))
+         (c-ts-mode      . lsp-deferred)
+         (c++-ts-mode    . lsp-deferred)
+         (js-ts-mode     . lsp-deferred)
+         (csharp-ts-mode . lsp-deferred)
          (lsp-mode . lsp-headerline-breadcrumb-mode)
          (lsp-headerline-breadcrumb-mode . batunii/lsp-modeline-setup))
   :init
   (setq lsp-keymap-prefix "C-c l")
   :custom
   (read-process-output-max (* 1024 1024))
+  (lsp-rust-analyzer-proc-macro-enable t)
+  (lsp-rust-analyzer-cargo-load-out-dirs-from-check t)
   :config
   (lsp-enable-which-key-integration)
- (setq lsp-diagnostic-level :error) 
-  ;; Breadcrumb configuration
+  (setq lsp-diagnostic-level :error)
+
   (setq lsp-headerline-breadcrumb-enable t
         lsp-headerline-breadcrumb-segments '(symbols))
-  
-  ;; Signature and documentation settings
+
   (setq lsp-signature-auto-activate t
         lsp-signature-render-documentation nil
         lsp-eldoc-enable-signature-help t
         eldoc-idle-delay 0.1
         eldoc-echo-area-use-multiline-p t
         max-mini-window-height 0.5)
-  
-  ;; Clean up mode-line
+
+  (setq lsp-eldoc-render-all nil)
+  (setq lsp-hover-to-eldoc t)
+  (add-hook 'lsp-mode-hook
+            (lambda ()
+              (add-hook 'eldoc-documentation-functions
+                        #'lsp-eldoc-function nil t)))
+
+  (add-hook 'csharp-ts-mode-hook
+            (lambda ()
+              (setq-local lsp-signature-render-documentation t)))
+
   (setq lsp-modeline-code-actions-enable nil
         lsp-modeline-diagnostics-enable nil
-        lsp-modeline-workspace-status-enable nil)
+        lsp-modeline-workspace-status-enable nil))
 
-)
+(use-package apheleia
+  :ensure t)
 
-;; Move breadcrumbs to mode-line
+(use-package lsp-java
+  :ensure t)
+
+(defun batunii/python-lsp-restart ()
+  "Re-detect Python interpreter via pet and restart LSP."
+  (interactive)
+  (setq-local lsp-pyright-python-executable-cmd
+              (pet-executable-find "python"))
+  (setq-local lsp-pyright-venv-path
+              (pet-virtualenv-root))
+  (lsp-restart-workspace))
+
 (defun batunii/lsp-modeline-setup ()
   "Move LSP breadcrumbs to mode-line after buffer name."
   (setq-local header-line-format nil)
   (setq-local mode-line-format
               (list
-               ;; Left side
                "%e"
                mode-line-front-space
                mode-line-mule-info
                mode-line-client
                mode-line-modified
                mode-line-remote
+               'vc-mode
+               " > "
                mode-line-frame-identification
                mode-line-buffer-identification
-               " "
-               ;; LSP Breadcrumbs
                '(:eval (when lsp-headerline-breadcrumb-mode
                          (lsp-headerline--build-string)))
                " "
-               ;; Right side
                mode-line-position
+               " "
                (vc-mode vc-mode)
                " "
                mode-line-modes
                mode-line-misc-info
                mode-line-end-spaces)))
 
-;; Show docs in minibuffer
 (defun batunii/lsp-show-doc ()
   "Show LSP documentation in minibuffer."
   (interactive)
@@ -334,14 +359,14 @@
 (use-package treemacs
   :ensure t)
 
-  (use-package lsp-treemacs
+(use-package lsp-treemacs
   :ensure t
   :after lsp
   :config
   (lsp-treemacs-sync-mode 1))
 
-(use-package vertico 
-  :ensure t 
+(use-package vertico
+  :ensure t
   :init (vertico-mode))
 
 (use-package orderless
@@ -354,22 +379,21 @@
   :hook (prog-mode . company-mode)
   :bind (:map company-active-map
          ("<tab>" . company-complete-selection)
-         ("C-n" . company-select-next)
-         ("C-p" . company-select-previous))
+         ("C-n"   . company-select-next)
+         ("C-p"   . company-select-previous))
   :custom
-  (company-minimum-prefix-length 2)
+  (company-minimum-prefix-length 0)
   (company-idle-delay 0.2)
   (company-selection-wrap-around t)
   (company-tooltip-align-annotations t)
   (company-require-match nil)
   (company-backends '(company-capf)))
 
-;; Hide additional minor modes from mode-line
-  (use-package diminish
-    :ensure t
-    :config
-(diminish 'eldoc-mode)
-(diminish 'visual-line-mode))
+(use-package diminish
+  :ensure t
+  :config
+  (diminish 'eldoc-mode)
+  (diminish 'visual-line-mode))
 
 (use-package wdired
   :ensure nil
@@ -382,7 +406,7 @@
   :after evil
   :hook (prog-mode . drag-stuff-mode)
   :config
-  (global-set-key (kbd "C-<up>") 'drag-stuff-up)
+  (global-set-key (kbd "C-<up>")   'drag-stuff-up)
   (global-set-key (kbd "C-<down>") 'drag-stuff-down))
 
 (use-package transient
@@ -392,19 +416,215 @@
   :ensure t
   :commands magit-status)
 
-(defun batunii/load-file()
-    "Load a file."
+(use-package marginalia
+  :ensure t
+  :after vertico
+  :init
+  (marginalia-mode 1))
+
+(defun batunii/load-file ()
+  "Load a file."
   (interactive)
   (load-file "~/.config/emacs/config.el"))
 
-(defun batunii/reload-config()
+(defun batunii/reload-config ()
   "Reload the config."
   (interactive)
   (org-babel-tangle "~/.config/emacs/config.org")
   (load-file "~/.config/emacs/config.el"))
 
-;; Enable native compilation if available
 (when (and (fboundp 'native-comp-available-p)
            (native-comp-available-p))
-  (setq native-comp-async-report-warnings-errors nil  ; Silence warnings
-        native-comp-deferred-compilation t))  ; Compile packages in background
+  (setq native-comp-async-report-warnings-errors nil
+        native-comp-deferred-compilation t))
+
+(use-package tldr
+  :ensure t)
+
+(use-package glsl-mode
+  :ensure t)
+
+(with-eval-after-load 'lsp-mode
+  (setq lsp-disabled-clients '(glslls))
+
+  (lsp-register-client
+   (make-lsp-client
+    :new-connection
+    (lsp-stdio-connection
+     '("/usr/local/bin/glsl_analyzer"))
+    :activation-fn (lsp-activate-on "glsl")
+    :server-id 'glsl-analyzer)))
+
+(add-to-list 'display-buffer-alist
+             '("\\*compilation\\*"
+               (display-buffer-at-bottom)
+               (window-height . 0.3)))
+
+(defun batunii/select-compilation-window (_proc)
+  "Select the compilation window after it is displayed."
+  (when-let ((win (get-buffer-window "*compilation*" t)))
+    (select-window win)))
+
+(add-hook 'compilation-start-hook #'batunii/select-compilation-window)
+
+(setq treesit-language-source-alist
+      '((cmake "https://github.com/uyha/tree-sitter-cmake" "master" "src")))
+
+(defun batunii/load-env-file (file)
+  (dolist (line (split-string (with-temp-buffer
+                                (insert-file-contents file)
+                                (buffer-string)) "\n" t))
+    (when (string-match "^\\([^=]+\\)=\\(.+\\)$" line)
+      (setenv (match-string 1 line) (match-string 2 line)))))
+
+(defun batunii/refresh-env ()
+  (interactive)
+  (batunii/load-env-file "~/.emacs-env")
+  (message "ENVS have been refreshed"))
+
+(batunii/load-env-file "~/.emacs-env")
+(setq exec-path (split-string (getenv "PATH") ":"))
+
+(setq arduino-executable (executable-find "arduino-cli"))
+
+(with-eval-after-load 'lsp-mode
+  (add-to-list 'lsp-language-id-configuration
+               '(arduino-mode . "arduino"))
+
+  (lsp-register-client
+   (make-lsp-client
+    :new-connection (lsp-stdio-connection
+                     (lambda ()
+                       (list "arduino-language-server"
+                             "-clangd" (executable-find "clangd")
+                             "-cli"    (executable-find "arduino-cli")
+                             "-cli-config" (expand-file-name
+                                            "~/.arduino15/arduino-cli.yaml")
+                             "-fqbn" "esp32:esp32:esp32c6")))
+    :major-modes '(arduino-mode c++-mode)
+    :server-id 'arduino-ls)))
+
+(defvar my/arduino-fqbn "esp32:esp32:esp32c6")
+(defvar my/arduino-port "/dev/ttyACM0")
+
+(defun my/arduino-sketch-dir ()
+  (if (buffer-file-name)
+      (file-name-directory (buffer-file-name))
+    (error "No file associated with this buffer")))
+
+(defun my/arduino-verify-override ()
+  (interactive)
+  (compile (format "arduino-cli compile --fqbn %s %s"
+                   my/arduino-fqbn (my/arduino-sketch-dir))))
+
+(defun my/arduino-upload-override ()
+  (interactive)
+  (compile (format "arduino-cli upload --fqbn %s --port %s %s"
+                   my/arduino-fqbn my/arduino-port (my/arduino-sketch-dir))))
+
+(with-eval-after-load 'arduino-mode
+  (advice-add 'arduino-verify :override #'my/arduino-verify-override)
+  (advice-add 'arduino-upload :override #'my/arduino-upload-override))
+
+;; ===================== ORG-ROAM SETUP =====================
+
+(use-package org
+  :ensure nil
+  :config
+  (setq org-directory "~/Documents/Code/org/"
+        org-hide-emphasis-markers t
+        org-return-follows-link t
+        org-startup-indented t))
+
+(use-package org-roam
+  :ensure t
+  :init
+  (setq org-roam-v2-ack t)
+  :custom
+  (org-roam-directory (file-truename "~/Documents/Code/org/roam/"))
+  (org-roam-completion-everywhere t)
+  :bind (("C-c n l" . org-roam-buffer-toggle)
+         ("C-c n f" . org-roam-node-find)
+         ("C-c n g" . org-roam-graph)
+         ("C-c n i" . org-roam-node-insert)
+         ("C-c n c" . org-roam-capture)
+         ("C-c n j" . org-roam-dailies-capture-today))
+  :config
+  (org-roam-db-autosync-mode)
+
+  ;; Capture templates
+  (setq org-roam-capture-templates
+        '(("d" "default" plain
+           "%?"
+           :if-new (file+head "${slug}.org"
+                              "#+title: ${title}\n#+date: %U\n")
+           :unnarrowed t))))
+
+;; ===================== ORG-ROAM UI (GRAPH) =====================
+
+(use-package org-roam-ui
+  :ensure t
+  :after org-roam
+  :config
+  (setq org-roam-ui-sync-theme t
+        org-roam-ui-follow t
+        org-roam-ui-update-on-save t
+        org-roam-ui-open-on-start t))
+
+;; ===================== MERMAID SUPPORT =====================
+
+(use-package ob-mermaid
+  :ensure t
+  :after org
+  :config
+  (setq ob-mermaid-cli-path "/home/batunii/tools/mermaid-cli/node_modules/.bin/mmdc")
+  (setq ob-mermaid-puppeteer-config-file
+      "/home/batunii/.config/mermaid/puppeteer.json")
+  (org-babel-do-load-languages
+   'org-babel-load-languages
+   '((mermaid . t)
+     (python . t)
+     (shell . t)))
+
+  (setq org-confirm-babel-evaluate nil))
+
+;; ===================== CITATIONS (OPTIONAL BUT RECOMMENDED) =====================
+
+(use-package citar
+  :ensure t
+  :custom
+  (citar-bibliography '("~/org/references.bib"))
+  :bind (("C-c b" . citar-open)))
+
+;; ===================== QUICK QUALITY OF LIFE =====================
+
+(setq org-id-link-to-org-use-id t)
+
+(defun batunii/org-roam-open-ui ()
+  "Open org-roam-ui graph in browser."
+  (interactive)
+  (org-roam-ui-mode 1))
+
+;; Unity integration
+(elpaca (unity :repo "elizagamedev/unity.el" :host github)
+  (require 'unity))
+(use-package shader-mode
+  :ensure t)
+(use-package lsp-shader
+  :ensure t
+  :after lsp-mode)
+(with-eval-after-load 'lsp-mode
+  (add-to-list 'lsp-language-id-configuration '("\\.shader\\'" . "shaderlab"))
+  (add-to-list 'lsp-language-id-configuration '(shader-mode . "shaderlab")))
+(defun batunii/shader-setup ()
+  (interactive)
+  (shader-mode)
+  (require 'lsp-shader)  ;; must happen before lsp
+  (lsp))
+
+(use-package vterm
+  :ensure t
+  )
+  ;; codex-cli.el via vc-use-package or straight.el
+(use-package codex-cli
+  :vc (:url "https://github.com/bennfocus/codex-cli.el" :rev :newest))
